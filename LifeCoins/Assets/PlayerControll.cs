d2505a71051a4c5ced4c704e53c86f506c263f58
Agreafel <csliw30@gmail.com>
2017-07-05 16:03:34 -0700

    sprite and C# file

---
+++ LifeCoins/Assets/PlayerControll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		//abc i like to pee
+
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("a"))
+        {
+        
+
+
+        }
+
+    }
+}
